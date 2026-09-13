9afaf1126396cc111dac09273b6ca81bbf871dda
shubhamnagar-source <nagarshubham2006@gmail.com>
2026-09-13 10:18:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function startWebsite() {
+    const message = document.getElementById("message");
+    const button = document.querySelector("button");
+
+    message.innerText = "🎉 Welcome! Your website is working perfectly!";
+    button.innerText = "Started ✓";
+}
